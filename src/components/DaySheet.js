31ae5aaafc458f8574add60e81b5db3d4889e40b
Dylan Rowe <dylanthk@gmail.com>
2020-09-11 15:45:52 -0500

    got stuff to display to the dom in React yipieee

---
+++ src/components/DaySheet.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { TourList } from "./Tour/TourList"
+import './DaySheet.css'
+// import { Login } from "./auth/Login"
+// import { Register } from "./auth/Register"
+
+
+export const DaySheet = () => ( 
+ <>
+ <article>
+<TourList />
+ </article>
+</>
+)
---
+++ src/components/Tour/TourList.js
@@ -0,0 +1,19 @@
+import React from "react"
+import './Tour.css'
+export const TourList = () => {
+    return <>
+        <section className="tour__Card">
+            <div>concert date</div>
+            <div>venue location</div>
+            <div>promoter Contact</div>
+            <div>load In</div>
+            <div>soundCheckTime</div>
+            <div>catering included?</div>
+            <div>buyout?</div>
+            <div>set Time</div>
+            <div>hotel location</div>
+            <button>delete</button>
+            <button>edit</button>
+        </section>
+    </>
+}
